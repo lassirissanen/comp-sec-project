5caed5e8e5c4a80ce8c1d2b60b31f3b7e4322534
Lassi Rissanen <82208000+lassirissanen@users.noreply.github.com>
2025-03-21 16:03:04 +0200

    Delete custom User model

--- project/securityforum/models.py
+++ project/securityforum/models.py
@@ -1,13 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-
-class User(models.Model):
-    username = models.CharField(max_length=22, unique=True, primary_key=True)
-
-    def __str__(self):
-        return f"username: {self.username}"
 
 
 class Post(models.Model):
