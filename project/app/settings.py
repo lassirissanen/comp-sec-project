1f7dd0e5b6fb1320eeda6746663f1ed06e176e0a
Lassi Rissanen <82208000+lassirissanen@users.noreply.github.com>
2025-04-01 13:53:41 +0300

    Argon2 password hashing

--- project/app/settings.py
+++ project/app/settings.py
@@ -103,6 +103,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Password hashing
+# https://docs.djangoproject.com/en/5.1/ref/settings/#password-hashers
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
