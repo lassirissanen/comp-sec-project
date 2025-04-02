38a7df115f21b4681b53d2e33eefed2cdf781862
Lassi Rissanen <82208000+lassirissanen@users.noreply.github.com>
2025-04-02 12:53:00 +0300

    Comment model created

--- project/securityforum/models.py
+++ project/securityforum/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,7 +9,17 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     header = models.CharField(max_length=50)
     content = models.CharField(max_length=250)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField("date published", default=datetime.now)
 
     def __str__(self):
         return f"ID: {self.id}, author: {self.author}, header: '{self.header}', content: '{self.content}', pub_date: {self.pub_date.isoformat()}"
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.CharField(max_length=250)
+    pub_date = models.DateTimeField("date published", default=datetime.now)
+
+    def __str__(self):
+        return f"author: {self.author.id}, content: {self.content}, pub_date: {self.pub_date.isoformat()}"
