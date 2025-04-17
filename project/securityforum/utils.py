d8bf14098a065120968bd14ac831eefaf5ee6eb7
Lassi Rissanen <82208000+lassirissanen@users.noreply.github.com>
2025-04-17 14:15:03 +0300

    Fix parameter type hint

--- project/securityforum/utils.py
+++ project/securityforum/utils.py
@@ -3,5 +3,5 @@ import string
 
 
 
-def get_random_string(length: str) -> str:
+def get_random_string(length: int) -> str:
   return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
